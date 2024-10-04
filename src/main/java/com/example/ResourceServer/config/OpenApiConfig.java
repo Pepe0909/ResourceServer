1745c0d60bef69f843fc267d72bc27702a3d3020
zhangkai <1158424575@qq.com>
2024-10-04 13:56:35 +0800

    grant type is authorization code

--- src/main/java/com/example/ResourceServer/config/OpenApiConfig.java
+++ src/main/java/com/example/ResourceServer/config/OpenApiConfig.java
@@ -1,9 +1,11 @@
 package com.example.ResourceServer.config;
 
+
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.*;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -39,18 +41,61 @@ public class OpenApiConfig {
     @Value("${spring.security.oauth2.client.provider.my-auth-server.token-uri}")
     private String tokenUrl;
 
+
+    @Value("${auth-server.url}")
+    private String authServerUrl;
+
+//    @Bean
+//    public OpenAPI customOpenAPI() {   // client credentials type
+//        return new OpenAPI()
+//                .components(new Components()
+//                        .addSecuritySchemes("oauth2", new SecurityScheme()
+//                                .type(SecurityScheme.Type.OAUTH2)
+//                                .flows(new OAuthFlows()
+//                                        .clientCredentials(new OAuthFlow()
+//                                                .tokenUrl(tokenUrl)
+//                                                .scopes(new Scopes()
+//                                                        .addString("openid", "OpenID Connect scope"))))))
+//                .addSecurityItem(new SecurityRequirement().addList("oauth2"));
+//    }
+
+
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
+                .info(new Info().title("Your API Title").version("1.0.0"))
                 .components(new Components()
                         .addSecuritySchemes("oauth2", new SecurityScheme()
                                 .type(SecurityScheme.Type.OAUTH2)
                                 .flows(new OAuthFlows()
-                                        .clientCredentials(new OAuthFlow()
-                                                .tokenUrl(tokenUrl)
+                                        .authorizationCode(new OAuthFlow()
+                                                .authorizationUrl(authServerUrl + "/oauth2/authorize")
+                                                .tokenUrl(authServerUrl + "/oauth2/token")
+                                                .refreshUrl(authServerUrl + "/oauth2/token")
                                                 .scopes(new Scopes()
-                                                        .addString("openid", "OpenID Connect scope"))))))
-                .addSecurityItem(new SecurityRequirement().addList("oauth2"));
+                                                        .addString("openid", "OpenID Connect scope")
+                                                     )))))
+                .addSecurityItem(new SecurityRequirement().addList("oauth2", Arrays.asList("openid")));
     }
+
+
+//    @Bean
+//    public OpenAPI customOpenAPI() {  // authorization code type
+//        return new OpenAPI()
+//                .info(new Info().title("Your API Title").version("1.0.0"))
+//                .addServersItem(new Server().url("/"))
+//                .schemaRequirement("OAuth2", createOAuthScheme());
+//    }
+//
+//    private SecurityScheme createOAuthScheme() {
+//        OAuthFlow authorizationCodeFlow = new OAuthFlow()
+//                .authorizationUrl(authServerUrl + "/oauth2/authorize")
+//                .tokenUrl(authServerUrl + "/oauth2/token")
+//                .refreshUrl(authServerUrl + "/oauth2/token");
+//
+//        return new SecurityScheme()
+//                .type(SecurityScheme.Type.OAUTH2)
+//                .flows(new OAuthFlows().authorizationCode(authorizationCodeFlow));
+//    }
 
 }
